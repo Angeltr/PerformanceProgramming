3ea83f41031474bcb9cfb9e5eb0d9557d726412b
Angel <trigasangelos@gmail.com>
2014-08-07 10:55:29 +0100

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,36 +1,28 @@
-class example_functor {
- /* ptr is an accessor to a global buffer. 
-  * The buffer itself will be assigned when creating the class.
-  */
- accessor<int, 1, access::read_write, access::global_buffer> ptr;
- /* random_num is a normal attribute for the functor, that will
-  * be initialized with a local value on the construction of the 
-  * functor
-  */
- int random_num;
+class vadd {
+ 
+ accessor<int, 1, access::read, access::global_buffer> a;
+ accessor<int, 1, access::read, access::global_buffer> b;
+ accessor<int, 1, access::write, access::global_buffer> c;
+ 
+ int size;
 
  public:
-  /* The constructor of the functor takes the accessor as a parameter.
-   */
-  example_functor(
-      accessor<int, 1, access::read_write, access::global_buffer> p)
-      : ptr(p) {
-        /* Generate a random number between 1 and 100.
-         * This is executed on the host.
-         */
-        random_num = std::rand()%(100-1)+1;
-      }
+  
+  vadd(
+       accessor<int, 1, access::read_write, access::global_buffer> A,
+       accessor<int, 1, access::read_write, access::global_buffer> B,
+       accessor<int, 1, access::read_write, access::global_buffer> C,
+       int N
+      )
+      : ptr(p), a(A), b(B), c(C), size(N) {}
 
-  /* The overload of the operator is used to execute the kernel
-   */
+  
   void operator()(item item) {
-    ptr[item.get_global()] = random_num;
+    int i = item.get_global_id(0);
+    
+    if (i < size) {
+      c[i] = a[i] + b[i];
+    }
   }
 
-  /* Kernel functors are normal C++ classes, therefore, they can have
-   * other methods
-   */
-  int get_random() {
-    return random_num;
-  }
 };
