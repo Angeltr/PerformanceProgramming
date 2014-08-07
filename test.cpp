98479cf7f435b132a13cc0f4d691d813b5400ab6
Angel <trigasangelos@gmail.com>
2014-08-07 11:01:33 +0100

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,17 +1,18 @@
+template <class T>
 class vadd {
  
- accessor<int, 1, access::read, access::global_buffer> a;
- accessor<int, 1, access::read, access::global_buffer> b;
- accessor<int, 1, access::write, access::global_buffer> c;
+ accessor<T, 1, access::read, access::global_buffer> a;
+ accessor<T, 1, access::read, access::global_buffer> b;
+ accessor<T, 1, access::write, access::global_buffer> c;
  
  int size;
 
  public:
   
   vadd(
-       accessor<int, 1, access::read_write, access::global_buffer> A,
-       accessor<int, 1, access::read_write, access::global_buffer> B,
-       accessor<int, 1, access::read_write, access::global_buffer> C,
+       accessor<T, 1, access::read, access::global_buffer> A,
+       accessor<T, 1, access::read, access::global_buffer> B,
+       accessor<T, 1, access::write, access::global_buffer> C,
        int N
       )
       : ptr(p), a(A), b(B), c(C), size(N) {}
