42f290226b645ec2aaa1600f848e53b61e41c5ab
Angel <trigasangelos@gmail.com>
2014-08-21 09:51:51 +0100

    Update sycl.cpp

--- sycl.cpp
+++ sycl.cpp
@@ -1,3 +1,29 @@
+template <class T>
+class vadd {
+	accessor<T, 1, access::read, access::global_buffer> a;
+	accessor<T, 1, access::read, access::global_buffer> b;
+	accessor<T, 1, access::write, access::global_buffer> c;
+	int size;
+public:
+	vadd(
+	     accessor<T, 1, access::read, access::global_buffer> A,
+	     accessor<T, 1, access::read, access::global_buffer> B,
+	     accessor<T, 1, access::write, access::global_buffer> C,
+	     int N
+	    ) : a(A), b(B), c(C), size(N) {}
+
+	void operator()(item item) {
+		int i = item.get_global_id(0);
+		if (i < size) {
+			c[i] = a[i] + b[i];
+		}
+	}
+}
+
+
+
+
+
 {		// SYCL region starts here
 	phi_selector selector;
   queue myQueue(selector);
